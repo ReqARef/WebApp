6317af6d8fac9fcd768376f5a83e06d82695f828
Kanav <khajuriakanav5@gmail.com>
2021-06-25 22:36:59 +0530

    Accept-Reject buttosn fixed

--- src/Components/RequestBox/Request/Request.js
+++ src/Components/RequestBox/Request/Request.js
@@ -7,29 +7,35 @@ const request = (props) => {
 		<div style={{
 			backgroundColor  : colors.white,
 			borderRadius : inlineStyles.borderRadius,
-			border : "1px solid " + colors.background
+			border : "1px solid " + colors.background,
+            display: "flex",
+            alignItems: "center",
+            flex: 1
 		}}
 			className={style.Request}
 		>
-			<img src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-business-male-icon-vector-png-image_4187852.jpg" alt="user"
-				className={style.UserImage}
-			></img>
-			<div className={style.UserData}>
-				<h3 className={style.userDataPara}>{props.userName}</h3>
-				<p className={style.userDataPara}>{props.designation + " "+props.companyName}</p>
-				<p className={style.userDataCountry}>{props.country}</p>
-			</div>
+            <div style={{display: 'flex', flex: 1}}>
+                <img src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-business-male-icon-vector-png-image_4187852.jpg" alt="user"
+                    className={style.UserImage}
+                ></img>
+                <div className={style.UserData}>
+                    <h3 className={style.userDataPara}>{props.userName}</h3>
+                    <p className={style.userDataPara}>{props.designation + " "+props.companyName}</p>
+                    <p className={style.userDataCountry}>{props.country}</p>
+                </div>
+            </div>
+			
 			<div className={style.ButtonDiv}>
 				<div className={style.AcceptDiv}
 					style={{
-						backgroundColor : colors.acceptButtonColor,
+						backgroundColor : colors.dark,
 						borderRadius : inlineStyles.borderRadius
 					}}
 				>
 					Accept
 				</div>
 				<div style={{
-					backgroundColor : colors.rejectButtonColor,
+					backgroundColor : colors.background,
 					borderRadius : inlineStyles.borderRadius
 				}}
 				className={style.RejectDiv}>
