e122b9456b005f07ed0bdbaa6c0fd8da737441c7
rsalwan98 <salwanrohit1998@gmail.com>
2021-07-08 21:54:13 +0530

    rebased

--- src/Components/RequestForm/RequestForm.js
+++ src/Components/RequestForm/RequestForm.js
@@ -6,7 +6,6 @@ import { useDispatch , useSelector } from 'react-redux'
 import { makeRequestAsync } from '../../store/actions/Request'
 import Loader from '../Loader/Loader'
 const RequestForm = (props) => {
-<<<<<<< HEAD
 	const dispatch = useDispatch();
 	const email = useSelector(state => state).requestReducer.requestTo;
 	const showLoader = useSelector(state => state).requestReducer.showLoader;
@@ -81,93 +80,6 @@ const RequestForm = (props) => {
 			</form>
 		</div>
 	)
-=======
-    const dispatch = useDispatch()
-    const email = useSelector((state) => state).requestReducer.requestTo
-    const token = useSelector((state) => state).User.authToken
-
-    const [jobId, setJobId] = useState('')
-    const [jobUrl, setJobUrl] = useState('')
-    const [comments, setComments] = useState('')
-
-    function onSubmitHandler(event) {
-        event.preventDefault()
-        dispatch(
-            makeRequestAsync(
-                jobId,
-                jobUrl,
-                comments,
-                email,
-                props.companyName,
-                token
-            )
-        )
-    }
-
-    function jobIdHandler(event) {
-        setJobId(event.target.value)
-    }
-
-    function jobUrlHandler(event) {
-        setJobUrl(event.target.value)
-    }
-
-    function commentsHandler(event) {
-        setComments(event.target.value)
-    }
-
-    return (
-        <div className={styles.requestDiv}>
-            <h2>ReqARef</h2>
-            <form>
-                <input
-                    className={styles.inputText}
-                    onChange={jobIdHandler}
-                    value={jobId}
-                    style={{
-                        backgroundColor: colors.white,
-                        color: colors.dark
-                    }}
-                    type="text"
-                    placeholder="Job Id"
-                ></input>
-                <input
-                    className={styles.inputText}
-                    onChange={jobUrlHandler}
-                    value={jobUrl}
-                    style={{
-                        backgroundColor: colors.white,
-                        color: colors.dark
-                    }}
-                    type="text"
-                    placeholder="Job URL"
-                ></input>
-                <input
-                    className={styles.inputText}
-                    onChange={commentsHandler}
-                    value={comments}
-                    style={{
-                        backgroundColor: colors.white,
-                        color: colors.dark
-                    }}
-                    type="text"
-                    placeholder="Comments"
-                ></input>
-                <div
-                    className={styles.requestButton}
-                    style={{
-                        backgroundColor: colors.dark,
-                        borderRadius: inlineStyles.borderRadius,
-                        cursor: 'pointer'
-                    }}
-                    onClick={(event) => onSubmitHandler(event)}
-                >
-                    Submit
-                </div>
-            </form>
-        </div>
-    )
->>>>>>> a8c535c7873dac34e41821d644ab8aa10f7df40c
 }
 
 export default RequestForm;
