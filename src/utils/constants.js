70c638e2e8e027c2888d6e0f0db5c18c574b9823
Kanav <khajuriakanav5@gmail.com>
2021-07-25 18:52:13 +0530

    Dev and staging env set

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,9 +6,7 @@ const authScreenParagraph =
     'This is a random string describing how good the company is and how it will get you a dream job'
 const imagePlaceHolder =
     'https://png.pngtree.com/png-clipart/20190520/original/pngtree-business-male-icon-vector-png-image_4187852.jpg'
-// const server = 'http://localhost:3001'
-const server = 'https://reqaref-backend.herokuapp.com'
-
+const server = process.env.REACT_APP_SERVER
 const companyLogoArray = [
     amdocsLogo,
     ciscoLogo,
